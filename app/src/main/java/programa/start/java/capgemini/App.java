36ea7baa96f0e9dd815a733d833ab52250bae60e
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-28 15:54:02 -0300

    Lista de exercícios 1 - ex2

--- app/src/main/java/programa/start/java/capgemini/App.java
+++ app/src/main/java/programa/start/java/capgemini/App.java
@@ -1,5 +1,6 @@
 /*
- * 1. Faça um algoritmo que receba dois números e exiba o resultado da sua soma;
+ * 2. Faça um algoritmo que receba dois números e ao final mostre a soma, subtração, multiplicação e a 
+ * divisão dos dois números lidos;
  */
 package programa.start.java.capgemini;
 
@@ -11,15 +12,22 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite os valores de X e Y para efeutuar a soma. ");
+        System.out.println(
+                "Digite os valores de X e Y para efeutuar os calculos (soma, subtração, multiplicação e divisão.). ");
         System.out.print("Digite X: ");
         int x = sc.nextInt();
         System.out.print("Digite Y: ");
         int y = sc.nextInt();
 
         int soma = x + y;
+        int subtracao = x - y;
+        int multiplicacao = x * y;
+        int divisao = x / y;
 
         System.out.println("A soma de X e Y é: " + soma);
+        System.out.println("A subtração de X e Y é: " + subtracao);
+        System.out.println("A multiplicação de X e Y é: " + multiplicacao);
+        System.out.println("A divisão de X e Y é: " + divisao);
 
         sc.close();
 
