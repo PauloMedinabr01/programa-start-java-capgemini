ccf3cfa07e70c990838090360259a99f0e38a5a7
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-29 09:18:28 -0300

    Lista de exercícios 1 - ex19

--- app/src/main/java/programa/start/java/capgemini/App.java
+++ app/src/main/java/programa/start/java/capgemini/App.java
@@ -1,6 +1,6 @@
 /*
- * 18. Faça um algoritmo que receba a idade de 75 pessoas e mostre mensagem informando “maior de idade” e
- * “menor de idade” para cada pessoa. Considere a idade a partir de 18 anos como maior de idade;
+ * 19. Escreva um algoritmo que leia o nome e o sexo de 56 pessoas e informe o nome e se ela é homem ou mulher.
+ * No final informe total de homens e de mulheres;
  */
 package programa.start.java.capgemini;
 
@@ -11,35 +11,42 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        int countMaior = 0;
-        int countMenor = 0;
-        int[] idadesMaiores = new int[75];
-        int[] idadesMenores = new int[75];
+        int countHomem = 0;
+        int countMulher = 0;
+        String[] homens = new String[56];
+        String[] mulheres = new String[56];
 
-        for (int i = 0; i < 75; i++) {
-            System.out.print("Informe uma idade: ");
-            int idade = sc.nextInt();
-            if (idade >= 18 && idade <= 75) {
-                idadesMaiores[countMaior] = idade;
-                countMaior++;
-            } else if (idade < 18) {
-                idadesMenores[countMenor] = idade;
-                countMenor++;
+        for (int i = 0; i < 56; i++) {
+            System.out.print("Informe o nome: ");
+            String nome = sc.nextLine();
+            System.out.print("Informe o sexo (M/F): ");
+            String sexo = sc.nextLine();
+
+            if (sexo.equalsIgnoreCase("M")) {
+                System.out.println(nome + " é um homem.");
+                homens[countHomem] = nome;
+                countHomem++;
+            } else if (sexo.equalsIgnoreCase("F")) {
+                System.out.println(nome + " é uma mulher.");
+                mulheres[countMulher] = nome;
+                countMulher++;
+            } else {
+                System.out.println("Sexo inválido, por favor informe novamente.");
+                i--;
             }
         }
 
-        System.out.println("Foram encontrados " + countMaior + " pessoas maiores de idade.");
-        System.out.println("Foram encontrados " + countMenor + " pessoas menores de idade.");
+        System.out.println("Foram encontrados " + countHomem + " homens.");
+        System.out.println("Foram encontrados " + countMulher + " mulheres.");
 
-        System.out.print("As idades dos maiores encontradas foram: ");
-        for (int i = 0; i < countMaior; i++) {
-            System.out.print(idadesMaiores[i] + " ");
+        System.out.print("Os homens são: ");
+        for (int i = 0; i < countHomem; i++) {
+            System.out.print(homens[i] + " ");
         }
         System.out.println();
-
-        System.out.print("As idades dos menores encontradas foram: ");
-        for (int i = 0; i < countMenor; i++) {
-            System.out.print(idadesMenores[i] + " ");
+        System.out.print("As mulheres são: ");
+        for (int i = 0; i < countMulher; i++) {
+            System.out.print(mulheres[i] + " ");
         }
         sc.close();
 
