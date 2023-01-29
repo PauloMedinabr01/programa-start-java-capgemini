234d52b27e41af6ddd30ee86aa145882c4c8430c
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-29 00:23:15 -0300

    Lista de exercícios 1 - ex18

--- app/src/main/java/programa/start/java/capgemini/App.java
+++ app/src/main/java/programa/start/java/capgemini/App.java
@@ -1,6 +1,6 @@
 /*
- * 16. Leia 80 números e ao final informar quantos número(s) est(á)ão no intervalo entre 10 (inclusive) e 150
- * (inclusive);
+ * 18. Faça um algoritmo que receba a idade de 75 pessoas e mostre mensagem informando “maior de idade” e
+ * “menor de idade” para cada pessoa. Considere a idade a partir de 18 anos como maior de idade;
  */
 package programa.start.java.capgemini;
 
@@ -11,23 +11,37 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        int count = 0;
-        int[] numeros = new int[80];
+        int countMaior = 0;
+        int countMenor = 0;
+        int[] idadesMaiores = new int[5];
+        int[] idadesMenores = new int[5];
 
-        for (int i = 0; i < 80; i++) {
-            System.out.print("Informe um número: ");
-            int numero = sc.nextInt();
-            if (numero >= 10 && numero <= 150) {
-                numeros[count] = numero;
-                count++;
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Informe uma idade: ");
+            int idade = sc.nextInt();
+            if (idade >= 18 && idade <= 75) {
+                idadesMaiores[countMaior] = idade;
+                countMaior++;
+            } else if (idade < 18) {
+                idadesMenores[countMenor] = idade;
+                countMenor++;
             }
         }
 
-        System.out.println("Foram encontrados " + count + " números no intervalo entre 10 e 150.");
-        System.out.print("Os números encontrados foram: ");
-        for (int i = 0; i < count; i++) {
-            System.out.print(numeros[i] + " ");
+        System.out.println("Foram encontrados " + countMaior + " pessoas maiores de idade.");
+        System.out.println("Foram encontrados " + countMenor + " pessoas menores de idade.");
+
+        System.out.print("As idades dos maiores encontradas foram: ");
+        for (int i = 0; i < countMaior; i++) {
+            System.out.print(idadesMaiores[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("As idades dos menores encontradas foram: ");
+        for (int i = 0; i < countMenor; i++) {
+            System.out.print(idadesMenores[i] + " ");
         }
         sc.close();
+
     }
 }
