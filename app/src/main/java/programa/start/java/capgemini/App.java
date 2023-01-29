5b257b68879ba32fb57ea36ba1d3a708bd4ca68b
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-29 15:20:50 -0300

    Revert "Lista de exercícios 1 - ex20"
    This reverts commit a1b68e1716d6346077c9687221c9c445cf1010f0.

--- app/src/main/java/programa/start/java/capgemini/App.java
+++ app/src/main/java/programa/start/java/capgemini/App.java
@@ -1,6 +1,9 @@
 /*
- * 21. Escreva um algoritmo que leia os dados de “N” pessoas (nome, sexo, idade e saúde) e informe se está
- *  apta ou não para cumprir o serviço militar obrigatório. Informe os totais;
+ * A concessionária de veículos “CARANGO VELHO” está vendendo os seus veículos com desconto. Faça 
+ * um algoritmo que calcule e exiba o valor do desconto e o valor a ser pago pelo cliente de vários carros. O 
+ * desconto deverá ser calculado de acordo com o ano do veículo. Até 2000 - 12% e acima de 2000 - 7%. O
+ * sistema deverá perguntar se deseja continuar calculando desconto até que a resposta seja: “(N) Não”. 
+ * Informar total de carros com ano até 2000 e total geral;
  */
 package programa.start.java.capgemini;
 
@@ -11,36 +14,43 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        int n, idade;
-        int contadorApto = 0;
-        int contadorInapto = 0;
-        String nome, sexo, saude;
+        int contadorCarrosAntigos = 0;
+        int contadorCarrosGeral = 0;
 
-        System.out.print("Quantas pessoas serão informadas: ");
-        n = sc.nextInt();
+        while (true) {
+            System.out.print("Informe o ano do carro: ");
+            int anoCarro = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("Pessoa " + (i + 1));
-            System.out.print("Nome: ");
-            nome = sc.next();
-            System.out.print("Sexo (M/F): ");
-            sexo = sc.next();
-            System.out.print("Idade: ");
-            idade = sc.nextInt();
-            System.out.print("Condição de saúde (apto/inapto): ");
-            saude = sc.next();
+            System.out.print("Informe o valor do carro: ");
+            double valorCarro = sc.nextDouble();
 
-            if (sexo.equalsIgnoreCase("M") && idade >= 18 && idade <= 45 && saude.equalsIgnoreCase("apto")) {
-                System.out.println(nome + " é apto para o serviço militar");
-                contadorApto++;
+            double valorDesconto;
+            if (anoCarro <= 2000) {
+                valorDesconto = valorCarro * 0.12;
+                contadorCarrosAntigos++;
+                contadorCarrosGeral++;
             } else {
-                System.out.println(nome + " não é apto para o serviço militar");
-                contadorInapto++;
+                valorDesconto = valorCarro * 0.07;
+                contadorCarrosGeral++;
             }
+
+            double valorAPagar = valorCarro - valorDesconto;
+
+            System.out.println("Valor do desconto: R$ " + String.format("%.2f", valorDesconto));
+            System.out.println("Valor a ser pago: R$ " + String.format("%.2f", valorAPagar));
+
+            System.out.print("Deseja continuar calculando desconto? (S/N): ");
+            String continuar = sc.next();
+
+            if (continuar.equalsIgnoreCase("N")) {
+                break;
+            }
+
         }
 
-        System.out.println("Total de pessoas aptas: " + contadorApto);
-        System.out.println("Total de pessoas inaptas: " + contadorInapto);
+        System.out.println("Total de carros com ano até 2000: " + contadorCarrosAntigos);
+        System.out.println("Total geral de carros: " + contadorCarrosGeral);
+
         sc.close();
 
     }
