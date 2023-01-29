3eee815c327c2c4e36188b3e58da86f3367bebbf
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-29 18:58:38 -0300

    Lista de exercícios 1 - ex24

--- app/src/main/java/programa/start/java/capgemini/App.java
+++ app/src/main/java/programa/start/java/capgemini/App.java
@@ -1,6 +1,5 @@
 /*
- * 23. Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 80, 
- * menor que 25 ou igual a 40;
+ * 24. Faça um algoritmo que receba “N” números e mostre positivo, negativo ou zero para cada número;
  */
 package programa.start.java.capgemini;
 
@@ -14,14 +13,12 @@ public class App {
         System.out.print("Digite um número: ");
         int n = sc.nextInt();
 
-        if (n > 80) {
-            System.out.println("Número " + n + " é maior que 80.");
-        } else if (n == 40) {
-            System.out.println("Número " + n + " é igual a 40.");
-        } else if (n < 25) {
-            System.out.println("Número " + n + " é menor que 25.");
+        if (n > 0) {
+            System.out.println("Número " + n + " é positivo.");
+        } else if (n < 0) {
+            System.out.println("Número " + n + " é negativo.");
         } else {
-            System.out.println("Número " + n + " não está nas condições.");
+            System.out.println("Número é igual a 0.");
         }
         sc.close();
     }
