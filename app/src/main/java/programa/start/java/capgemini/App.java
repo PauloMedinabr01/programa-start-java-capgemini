abe1606b453330ff8f6c898bb77aa51b9001ad62
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-29 19:42:26 -0300

    Lista de exercícios 1 - ex27

--- app/src/main/java/programa/start/java/capgemini/App.java
+++ app/src/main/java/programa/start/java/capgemini/App.java
@@ -1,6 +1,8 @@
 /*
- * 26. Faça um algoritmo que leia um número de 1 a 5 e escreva por extenso. Caso o usuário digite um número
- * que não esteja neste intervalo, exibir a seguinte mensagem: número inválido;
+ * 27. A concessionária de veículos “CARANGO” está vendendo os seus veículos com desconto. Faça um algoritmo
+ *  que calcule e exiba o valor do desconto e o valor a ser pago pelo cliente. O desconto deverá ser calculado
+ *  sobre o valor do veículo de acordo com o combustível (álcool – 25%, gasolina – 21% ou diesel –14%). 
+ * Com valor do veículo zero encerra entrada de dados. Informe total de desconto e total pago pelos clientes;
  */
 package programa.start.java.capgemini;
 
@@ -10,28 +12,45 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite um número de 1 a 5: ");
-        int num = sc.nextInt();
 
-        switch (num) {
-            case 1:
-                System.out.println("Um");
+        double totalDesconto = 0;
+        double totalPago = 0;
+
+        while (true) {
+            System.out.print("Informe o valor do veículo: ");
+            double valorVeiculo = sc.nextDouble();
+
+            if (valorVeiculo == 0) {
                 break;
-            case 2:
-                System.out.println("Dois");
-                break;
-            case 3:
-                System.out.println("Três");
-                break;
-            case 4:
-                System.out.println("Quatro");
-                break;
-            case 5:
-                System.out.println("Cinco");
-                break;
-            default:
-                System.out.println("Número inválido");
+            }
+
+            System.out.print("Informe o tipo de combustível (Álcool, Gasolina ou Diesel): ");
+            String combustivel = sc.next();
+
+            double desconto = 0;
+            if (combustivel.equalsIgnoreCase("Álcool")) {
+                desconto = 0.25;
+            } else if (combustivel.equalsIgnoreCase("Gasolina")) {
+                desconto = 0.21;
+            } else if (combustivel.equalsIgnoreCase("Diesel")) {
+                desconto = 0.14;
+            } else {
+                System.out.println("Combustível inválido");
+                continue;
+            }
+
+            double valorDesconto = valorVeiculo * desconto;
+            double valorPago = valorVeiculo - valorDesconto;
+
+            totalDesconto += valorDesconto;
+            totalPago += valorPago;
+
+            System.out.println("Valor do desconto: R$" + valorDesconto);
+            System.out.println("Valor a ser pago: R$" + valorPago);
         }
+
+        System.out.println("Total de desconto: R$" + totalDesconto);
+        System.out.println("Total pago pelos clientes: R$" + totalPago);
 
         sc.close();
     }
