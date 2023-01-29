50afc5d5ebb4aa7fb31c0cf05af12bc220313c62
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-28 22:26:20 -0300

    Lista de exercícios 1 - ex13

--- app/src/main/java/programa/start/java/capgemini/App.java
+++ app/src/main/java/programa/start/java/capgemini/App.java
@@ -1,35 +1,25 @@
 /*
- * 12. O custo de um carro novo ao consumidor é a soma do custo de fábrica mais o percentual do distribuidor e
- * dos impostos aplicados (primeiro os impostos são aplicados sobre o custo de fábrica, e depois o percentual
- * do distribuidor sobre o resultado). Supondo que o percentual do distribuidor seja de 28% e os impostos
- * 45%, escreva um algoritmo que leia o custo de fábrica de um carro e informe o custo ao consumidor do mesmo;
+ * 13. Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 10;
  */
 package programa.start.java.capgemini;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double custoFabrica, custoDistribuidor, custoImpostos, custoConsumidor;
+        System.out.println("Informe um número: ");
+        int numero = sc.nextInt();
 
-        final double PERC_DISTRIBUIDOR = 0.28;
-        final double PERC_IMPOSTOS = 0.45;
-
-        System.out.print("Informe o custo de fabrica do produto: R$");
-        custoFabrica = sc.nextDouble();
-
-        custoImpostos = custoFabrica * PERC_IMPOSTOS;
-        custoDistribuidor = (custoFabrica + custoImpostos) * PERC_DISTRIBUIDOR;
-        custoConsumidor = custoFabrica + custoImpostos + custoDistribuidor;
-
-        System.out.println("Custo ao consumidor: R$" + String.format("%.2f", custoConsumidor));
-
+        while (numero <= 10) {
+            System.out.println("Digite um numero maior que 10: ");
+            numero = sc.nextInt();
+            if (numero > 10) {
+                System.out.println("Numero: " + numero + " - maior que 10. ");
+            }
+        }
         sc.close();
     }
 }
